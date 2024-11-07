bcb9d035238dbc806ea68f303b450188b8059d8f
masatos1012 <masatosama1012.2@gmail.com>
2024-11-07 21:52:18 +0900

    会員登録ページの変更

--- accounts/views.py
+++ accounts/views.py
@@ -32,7 +32,5 @@ def login_view(request):
 
 
 def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('quiz:index')
-    return render(request, 'account/logout.html')
+    logout(request)
+    return redirect('quiz:index')
